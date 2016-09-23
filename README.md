b6b90027e2a56bd59a4279afabcbf52edf260fe6
Jan Hoersch <jhoersch@gmail.com>
2016-09-23 13:55:01 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # rocketchat-pushprivacy
+A lot of information get's send out to external server. 
+To prevent this, rocketchat-pushprivacy is a proxy to relay the push notification over your server to clean 
+from any unwanted data.
+
+Send Out:
+```
+{"token":"[some token]","options":{"_id":"CP......N6Pp","createdAt":"2016-08-31T11:10:03.930Z","createdBy":"<SERVER>","from":"push","title":"<some Title>","text":"<some Text>","payload":{"host":"https://rocketchat.local","rid":"5nQ...........6","sender":{"_id":"5n.....j","username":"nv1t"},"type":"d","name":null},"badge":1,"sound":"chime","apn":{"text":"<some Text>"},"query":{"userId":"ji2BWno....D6"}}}
+```
+
+Actualy needed:
+```
+{"token":"[some token]","options":{"badge":1,"sound":"chime","apn":{"text":"<some Text>"}}}
+```
+
+## Deploy
+```
+$ npm i rocketchat-pushprivacy
+$ rocketchat-pushprivacy -p 4444 -i 127.0.0.1 -m "Securai needs you!"
+```
+
+Now you can redirect push notification in your administration interface over your proxy :)
+Only the token and the defined proxy will get send out! 
---
+++ index.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+'use strict';
+
+var http = require('http'),
+    https = require('https'),
+    argv = require('optimist')
+      .usage('Usage: $0 -i [ip] -p [port] -m [message]')
+      .alias('i','ip')
+      .alias('m','message')
+      .alias('p','port')
+      .describe('i','IP Address to Listen on  [127.0.0.1]')
+      .describe('m','Message to send in Push Notification [Securai needs you!]')
+      .describe('p','Port to Listen on [4444]')
+      .default({
+        i:'127.0.0.1',
+        p:4444,
+        m:'Securai needs you!'
+      })
+      .argv;
+
+var log = (level,message) => {
+  console.log(new Date()+' - '+message)
+}
+
+http.createServer((req, res) => {
+  if(req.method == 'POST') {
+    var body = '';
+    req.on('data', (data) => {
+      body += data;
+    });
+
+    req.on('end', () => {
+      log('info','POST Request received')
+      var json = JSON.parse(body);
+      var data = JSON.stringify({'token':json['token'],'options': {'badge':1,'sound':'chime','apn': {'text':argv.message}}});
+
+
+      var notificationreq = https.request({
+        host: 'gateway.rocket.chat',
+        port: 443,
+        method: 'POST',
+        path: '/push/apn/send',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': Buffer.byteLength(data)
+        }
+      }, (res) => {
+        var result = '';
+        res.on('data', (chunk) => {
+          result += chunk;
+        });
+        res.on('end', () => {
+          log('info',result);
+        });
+        res.on('error', (err) => {
+          log('error',err);
+        })
+      });
+      // req error
+      notificationreq.on('error', (err) => {
+        log('error',err);
+      });
+      notificationreq.write(data);
+      notificationreq.end();
+    });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('post received');
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+  }
+}).listen(argv.port,argv.ip);
+
+log('info','RocketChat-PushPrivacy was started '+argv.ip+':'+argv.port+' sending message "'+argv.message+'"');
